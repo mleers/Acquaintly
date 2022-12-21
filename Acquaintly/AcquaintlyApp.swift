2354ddc330e865a942707538b7e82c4b60521dcc
mleers <mleers4@gmail.com>
2022-12-21 15:08:24 -0800

    Initial Commit

---
+++ Acquaintly/AcquaintlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AcquaintlyApp.swift
+//  Acquaintly
+//
+//  Created by Mitch on 12/21/22.
+//
+
+import SwiftUI
+
+@main
+struct AcquaintlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
